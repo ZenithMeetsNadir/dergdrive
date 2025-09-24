ef055b88498af4310577d65ec62964e575f0e938
ZenithMeetsNadir <martin-vlcek@post.cz>
2025-09-24 10:26:16 +0200

    run all tests on build

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const proto = struct {
     pub const SyncMessage = @import("proto/sync/SyncMessage.zig");
     pub const RequestChunk = @import("proto/sync/RequestChunk.zig");
@@ -5,7 +7,12 @@ pub const proto = struct {
 
 pub const crypt = @import("crypt/crypt.zig");
 pub const conf = @import("conf/conf.zig");
+pub const Env = @import("conf/Env.zig");
 
 pub const cli = struct {
     pub const prompt = @import("cli/prompt.zig");
 };
+
+test {
+    std.testing.refAllDecls(@This());
+}
